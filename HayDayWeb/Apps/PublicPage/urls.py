0278655cec111fac519d0e8504094bf17c90a939
pawas <pawas.s@skysoft.net.in>
2022-09-15 00:47:53 +0530

    update

--- HayDayWeb/Apps/PublicPage/urls.py
+++ HayDayWeb/Apps/PublicPage/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
 from .views import *
-from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', Home.as_view(), name="home_page"),
@@ -15,3 +16,5 @@ urlpatterns = [
     path('contact', Contact.as_view(), name="home_contact"),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
